c31d471d0988992750d3e4c54a216d9f33925b1b
Adam Krška <adam@krska.com>
2025-01-12 23:30:21 +0100

    refresh token for runner removal

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,19 @@ ACCESS_TOKEN=$TOKEN
 
 echo "REPO ${REPOSITORY}"
 
-REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" \
-	https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
+function get_reg_token() {
+	curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" \
+		https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output
+}
 
 cd /home/runner/actions-runner
 
+REG_TOKEN=$(get_reg_token)
 ./config.sh --unattended --url "https://github.com/${REPOSITORY}" --token "${REG_TOKEN}"
 
 cleanup() {
 	echo "Removing runner..."
+	REG_TOKEN=$(get_reg_token)
 	./config.sh remove --token "${REG_TOKEN}"
 }
 
